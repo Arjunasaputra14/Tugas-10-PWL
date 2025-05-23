83336ba662c0d47aea1aea08fb26646d253767dc
Arjunasaputra14 <ap0901466@gmail.com>
2025-05-23 22:12:08 +0700

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,25 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->post('produk', 'ProdukController::create', ['filter' => 'auth']);
+$routes->post('produk/edit/(:any)', 'ProdukController::edit/$1', ['filter' => 'auth']);
+$routes->get('produk/delete/(:any)', 'ProdukController::delete/$1', ['filter' => 'auth']);
+
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+
+$routes->get('Logout', 'AuthController::Logout');
+
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
